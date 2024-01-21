0c9236150a6c73fb7f439f3e830a565dd3cc2740
Concourse <concourse@finbourne.com>
2024-01-21 22:27:15 +0000

    Updates for v2.0.52

--- sdk/src/main/java/com/finbourne/luminesce/ApiClient.java
+++ sdk/src/main/java/com/finbourne/luminesce/ApiClient.java
@@ -208,7 +208,7 @@ public class ApiClient {
         json = new JSON();
 
         // Set default User-Agent.
-        setUserAgent("OpenAPI-Generator/2.0.51/java");
+        setUserAgent("OpenAPI-Generator/2.0.52/java");
 
         authentications = new HashMap<String, Authentication>();
     }
