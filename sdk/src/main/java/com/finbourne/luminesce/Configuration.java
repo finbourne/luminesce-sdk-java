ffe5a47e1e11ef7030f1d4ae8e27409eff46c32e
Concourse <concourse@finbourne.com>
2024-05-10 15:05:09 +0000

    Updates for v2.0.152

--- sdk/src/main/java/com/finbourne/luminesce/Configuration.java
+++ sdk/src/main/java/com/finbourne/luminesce/Configuration.java
@@ -12,7 +12,7 @@ package com.finbourne.luminesce;
 
 @jakarta.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen")
 public class Configuration {
-    public static final String VERSION = "2.0.151";
+    public static final String VERSION = "2.0.152";
 
     private static ApiClient defaultApiClient = new ApiClient();
 
