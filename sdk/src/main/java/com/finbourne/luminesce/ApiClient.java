188a7b0f5eba298c4b5f4a7b19f2f2302caa8676
Concourse <concourse@finbourne.com>
2024-05-20 17:27:08 +0000

    Updates for v2.0.161

--- sdk/src/main/java/com/finbourne/luminesce/ApiClient.java
+++ sdk/src/main/java/com/finbourne/luminesce/ApiClient.java
@@ -208,7 +208,7 @@ public class ApiClient {
         json = new JSON();
 
         // Set default User-Agent.
-        setUserAgent("OpenAPI-Generator/2.0.160/java");
+        setUserAgent("OpenAPI-Generator/2.0.161/java");
 
         authentications = new HashMap<String, Authentication>();
     }
--- sdk/src/main/java/com/finbourne/luminesce/Configuration.java
+++ sdk/src/main/java/com/finbourne/luminesce/Configuration.java
@@ -12,7 +12,7 @@ package com.finbourne.luminesce;
 
 @jakarta.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen")
 public class Configuration {
-    public static final String VERSION = "2.0.160";
+    public static final String VERSION = "2.0.161";
 
     private static ApiClient defaultApiClient = new ApiClient();
 
