8e2825214e6672c7a6171801fd5b7a20d17d164b
Concourse <concourse@finbourne.com>
2024-10-04 18:12:43 +0000

    luminesce java updates for v2.0.273
    Signed-off-by: Concourse <concourse@finbourne.com>

--- sdk/src/main/java/com/finbourne/luminesce/Configuration.java
+++ sdk/src/main/java/com/finbourne/luminesce/Configuration.java
@@ -12,7 +12,7 @@ package com.finbourne.luminesce;
 
 @jakarta.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen")
 public class Configuration {
-    public static final String VERSION = "2.0.272";
+    public static final String VERSION = "2.0.273";
 
     private static ApiClient defaultApiClient = new ApiClient();
 
