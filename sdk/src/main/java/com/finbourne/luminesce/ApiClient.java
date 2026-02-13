fc4abb5ad3090c2274e5c62eca8b71454d9886a8
Concourse <concourse@finbourne.com>
2026-02-13 20:38:22 +0000

    luminesce java updates for v2.2.26
    Signed-off-by: Concourse <concourse@finbourne.com>

--- sdk/src/main/java/com/finbourne/luminesce/ApiClient.java
+++ sdk/src/main/java/com/finbourne/luminesce/ApiClient.java
@@ -212,7 +212,7 @@ public class ApiClient {
         json = new JSON();
 
         // Set default User-Agent.
-        setUserAgent("OpenAPI-Generator/2.2.25/java");
+        setUserAgent("OpenAPI-Generator/2.2.26/java");
 
         authentications = new HashMap<String, Authentication>();
     }
